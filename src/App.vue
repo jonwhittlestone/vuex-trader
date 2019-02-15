92c986b6a20a05b7613fdbbc28d5de6754ec0c75
Jon Whittlestone <dev@howapped.com>
2019-02-15 06:14:05 +0000

    Setting up basic component and routes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,40 @@
 <template>
     <div class="container">
-      Stock Trader
-    </div>
 
-    <!--
+      <nav class="navbar navbar-default">
+          <div class="container-fluid">
+              <div class="navbar-header">
+                  Stock Trader
+              </div>
+
+              <div class="collapse navbar-collapse">
+                  <ul class="nav navbar-nav">
+                    <router-link to='/portfolio' tag='li'>
+                    <a> 
+                      Portfolio
+                    </a>  
+                     </router-link> 
+
+                    <router-link to='/stocks' tag='li'>
+                    <a> 
+                      Stocks
+                    </a>  
+                     </router-link> 
+                  </ul>
+              </div><!-- /.navbar-collapse -->
+          </div><!-- /.container-fluid -->
+      </nav>
+
+      <hr>
+
+      <router-view></router-view>
+
+    </div>
+</template>
+
+<script>
+
+    /*
         1. count components we need and create a template for each
           - Home
           - Header
@@ -11,11 +42,11 @@
           - stocks/Stock.vue
           - portfolio/Stock.vue
           - portfolio/Portfolio.vue
-      -->
-</template>
-
-<script>
-
+        2. Count and Set up routes
+          1. /#           // starting page
+          2. /stocks      // choose a stock
+          3. /portfolio   // after adding, we can see in portfolio
+    */
     export default {
 
     }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+import { routes } from './routes';
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+});
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
